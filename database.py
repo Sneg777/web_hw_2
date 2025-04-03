d515b6ea38aeb7d252a5fe54c53c9e77f73cc2cc
Yevhenii Bessonov <49118664+Sneg777@users.noreply.github.com>
2025-04-03 17:38:36 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,1 @@
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
 
-
-uri = "mongodb+srv://Sneg321:wersta159@clusterbes.tzjea.mongodb.net/?retryWrites=true&w=majority&appName=ClusterBes"
